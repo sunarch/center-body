6dd962f6ecb606e1d424cd9703850ba766b85230
András Németh <sunarch@protonmail.com>
2023-07-20 23:40:45 +0200

    Add second method and cycle on click
    For some pages auto margins will not work. Instead, adding padding
    to the html root is an alternative. Clicking the add-on button now
    cycles through the two options and a none-option.

--- src/center-body.js
+++ src/center-body.js
@@ -4,14 +4,45 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-let css = "body { width: fit-content; margin-left: auto; margin-right: auto; }";
+const css = [
+{title: "no change", css: ""},
+{title: "body margin auto", css: `
+body {
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+}
+`},
+{title: "html padding 30%", css: `
+html {
+    padding-left: 30%;
+    padding-right: 30%;
+}
+`}
+];
+
+let level = 0;
 
 browser.browserAction.onClicked.addListener(() => {
 
     function onError(error) {
-        console.log(`Error: ${error}`);
+        console.log(`Center Body - Error: ${error}`);
     }
 
-    let insertingCSS = browser.tabs.insertCSS({code: css});
-    insertingCSS.then(null, onError);
+    if (!(0 <= level < css.length)) {
+        return;
+    }
+    
+    browser.tabs
+        .removeCSS({code: css[level].css})
+        .then(null, onError);
+
+    level = (level + 1) % css.length;
+
+    browser.tabs
+        .insertCSS({code: css[level].css})
+        .then(null, onError);
+       
+    let new_title = `[${level + 1}/${css.length}] ${css[level].title}`;
+    browser.browserAction.setTitle({ title: new_title });
 });
